306eb9165e4113800f51ab51c19773e8835c7384
Jake Moening <jaboc83@gmail.com>
2018-06-07 13:38:00 -0500

    Changing the port logic to debug azure setup

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+console.log("PAST THE REQUIRE");
 
-app.get('/', (req, res) => res.send('Hello World!'))
+const port = process.env.port || 3000;
 
-app.listen(process.env.PORT||3000, () => console.log('Example app listening!'))
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.listen(port, () => console.log('Example app listening!'));
